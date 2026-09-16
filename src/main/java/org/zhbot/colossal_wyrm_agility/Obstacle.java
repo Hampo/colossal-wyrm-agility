568a6b6e7883ecf5d3b43c24f12fedcc64b1c1e2
Proddy <josh@zhbot.org>
2026-09-17 00:46:42 +0100

    fix/Incorrect obstacle duration

--- src/main/java/org/zhbot/colossal_wyrm_agility/Obstacle.java
+++ src/main/java/org/zhbot/colossal_wyrm_agility/Obstacle.java
@@ -5,34 +5,33 @@ import net.runelite.api.coords.WorldPoint;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 @Getter
 public enum Obstacle {
-    START_LADDER("Start Ladder", new WorldPoint(1652, 2931, 0), new WorldPoint(1652, 2931, 0), 1),
-    TIGHTROPE("Tightrope", new WorldPoint(1655, 2926, 1), new WorldPoint(1649, 2910, 1), 39),
+    START_LADDER("Start Ladder", 1, new WorldPoint(1652, 2931, 0), new WorldPoint(1652, 2931, 0)),
+    TIGHTROPE("Tightrope", 39, new WorldPoint(1649, 2910, 1), new WorldPoint(1655, 2926, 1)),
 
-    BASIC_TIGHTROPE("Basic Tightrope", new WorldPoint(1647, 2910, 1), new WorldPoint(1635, 2910, 1), 16),
-    ROPE("Rope", new WorldPoint(1632, 2910, 1), new WorldPoint(1627, 2931, 1), 25),
-    BASIC_LADDER("Basic Ladder", new WorldPoint(1626, 2932, 1), new WorldPoint(1626, 2932, 1), 1),
+    BASIC_TIGHTROPE("Basic Tightrope", 16, new WorldPoint(1635, 2910, 1), new WorldPoint(1647, 2910, 1)),
+    ROPE("Rope", 25, new WorldPoint(1627, 2931, 1), new WorldPoint(1632, 2910, 1)),
+    BASIC_LADDER("Basic Ladder", 1, new WorldPoint(1626, 2932, 1), new WorldPoint(1626, 2932, 1)),
 
-    ADVANCED_LADDER("Advanced Ladder", new WorldPoint(1648, 2909, 1), new WorldPoint(1648, 2909, 1), 1),
-    EDGE("Edge", new WorldPoint(1647, 2907, 2), new WorldPoint(1635, 2907, 2), 10),
-    ADVANCED_TIGHTROPE("Advanced Tightrope", new WorldPoint(1633, 2908, 2), new WorldPoint(1624, 2931, 2), 55),
-    ADVANCED_TIGHTROPE2("Advanced Tightrope", new WorldPoint(1634, 2908, 2), new WorldPoint(1624, 2931, 2), 56),
-    ADVANCED_TIGHTROPE3("Advanced Tightrope", new WorldPoint(1633, 2907, 2), new WorldPoint(1624, 2931, 2), 56),
-    ZIPLINE("Zipline", new WorldPoint(1625, 2933, 2), new WorldPoint(1645, 2933, 0), 13);
+    ADVANCED_LADDER("Advanced Ladder", 1, new WorldPoint(1648, 2909, 1), new WorldPoint(1648, 2909, 1)),
+    EDGE("Edge", 10, new WorldPoint(1635, 2907, 2), new WorldPoint(1647, 2907, 2)),
+    ADVANCED_TIGHTROPE("Advanced Tightrope", 56, new WorldPoint(1624, 2931, 2), new WorldPoint(1633, 2908, 2), new WorldPoint(1634, 2908, 2), new WorldPoint(1633, 2907, 2)),
+    ZIPLINE("Zipline", 13, new WorldPoint(1645, 2933, 0), new WorldPoint(1625, 2933, 2));
 
     private final String name;
-    private final WorldPoint startPoint;
+    private final Set<WorldPoint> startPoints;
     private final WorldPoint endPoint;
     private final int ticks;
 
-    Obstacle(String name, WorldPoint startPoint, WorldPoint endPoint, int ticks)
+    Obstacle(String name, int ticks, WorldPoint endPoint, WorldPoint... startPoints)
     {
         this.name = name;
-        this.startPoint = startPoint;
-        this.endPoint = endPoint;
         this.ticks = ticks;
+        this.endPoint = endPoint;
+        this.startPoints = Set.of(startPoints);
     }
 
     private static final Map<WorldPoint, Obstacle> START_POINT_MAP = new HashMap<>();
@@ -42,9 +41,10 @@ public enum Obstacle {
     {
         for (var obstacle : values())
         {
-            START_POINT_MAP.put(obstacle.getStartPoint(), obstacle);
+            for (var startPoint : obstacle.startPoints)
+                START_POINT_MAP.put(startPoint, obstacle);
 
-            END_POINT_MAP.put(obstacle.getEndPoint(), obstacle);
+            END_POINT_MAP.put(obstacle.endPoint, obstacle);
         }
     }
 
